a35e6d17d5f9e223b74178daf10496ad61308d4c
yixuefu <144550180+yixuefu@users.noreply.github.com>
2023-09-10 17:54:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,14 +5,14 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'http://doctor.cq.cn' },
+    { title: '访问文档中心获取更多帮助', url: 'http://doctor.cq.cn' }
   ],
 
   // 英雄区(首页顶部大卡)
   HERO_TITLE_1: '分享编程',
   HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
+  HERO_TITLE_3: 'doctor.cq.cn',
   HERO_TITLE_4: '新版上线',
   HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HERO_TITLE_LINK: 'https://tangly1024.com',
@@ -23,7 +23,7 @@ const CONFIG = {
   HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HERO_RECOMMEND_POST_TAG: '推荐',
+  HERO_RECOMMEND_POST_TAG: '',
   HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
